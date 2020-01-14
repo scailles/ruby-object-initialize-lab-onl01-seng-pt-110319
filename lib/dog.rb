577af3f0cb0e5d8fdc11f600b22c403af4938293
Shaina C <scailles23@gmail.com>
2020-01-14 05:19:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  def initialize(name, breed="Mutt")
+    @name=name
+    @breed=breed
+  end
+  
+  def initialize(name, breed="Mutt")
+    @name=name
+    @breed=breed
+  end
+
+  def dname
+  @name
+  end
+
+  def breed
+    @breed
+  end
+  
+end
+    
